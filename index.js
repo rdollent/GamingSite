fcb0c20c390bd53d65f11ebc21c497fc12ea3d8e
rdollent <rumbalumba89@gmail.com>
2017-04-07 14:09:21 -0600

    Update index.js
    - added getData() function, to successfully get JSON data from Giantbomb API.
    - added arrResults and storeData(), which stores the captured JSON into an array for use with populating HTML and results list.
    - added createList() function, which creates a list of results from the received JSON data, and displays the result on the page using DOM manipulation.
    - added removeList(), which clears the displayed list to make way for the game page to be displayed, by deleting both mainDiv and the added script tag when querying.
    - added createBackButton(), which places a back button when game content is loaded; purpose is to go back to results page from the game content page
    - added newPage() function, which takes the content from JSON data (a string with html tags) and displays it on the page, replacing results list. Modifies the string to exclude <a> tags and hyperlinks in the JSON content, by replacing <a> with <span> and removing href attributes.
    - added rebuildList() function to run both removeList() and createList() functions when click event fires in Back button.

--- index.js
+++ index.js
@@ -1,32 +1,107 @@
-///Giantbomb API key
+//Giantbomb API key
 //79fbb0e3b137824088774e8af118892b8cb22e6e
 
 const key = '79fbb0e3b137824088774e8af118892b8cb22e6e';
-
-function getData(s) {
-  console.log(s);
-}
+let answer;
 
 window.onload = runThis;
 
 function runThis() {
   const submit = document.getElementById('submit');
   const search = document.getElementById('search');
-  submit.addEventListener('click', clickThis);
-
-
+  submit.addEventListener('click', getData);
   
-  function clickThis() {
-    let ans = search.value;
+  function getData() {
+    answer = search.value;
     const jsonpScript = document.createElement('script');
-    jsonpScript.src = `https://www.giantbomb.com/api/search/?api_key=${key}&format=jsonp&json_callback=getData&query=${ans}&resources=game`;
+    jsonpScript.id = 'jsonpScriptId'
+    jsonpScript.src = `https://www.giantbomb.com/api/search/?api_key=${key}&format=jsonp&json_callback=storeData&query=${answer}&resources=game`;//&field_list=id,name,deck,image,original_release_date,platforms`;
     document.head.appendChild(jsonpScript);
-
   }
-    
-
-
-
-
 }
 
+let arrResults = [];
+//note: callback should be in global scope
+function storeData(s) {
+  console.log(s);
+  if(s.error === "OK") {  
+    arrResults = s.results;
+    console.log(arrResults);
+    //create divs for list
+    createList();
+ }
+}
+
+function createList() {
+  removeList();
+  let mainDiv = document.createElement('div');
+  mainDiv.id = 'mainDiv';
+  for(let i = 0; i < arrResults.length;i++) {
+    let newDiv = document.createElement('div');
+    newDiv.innerHTML = `<p><h2 class="titles">${arrResults[i].name}</h2></p><p>${arrResults[i].deck}</p>`;
+    newDiv.className = 'results';
+    newDiv.id = `newdiv${i}`;
+    mainDiv.appendChild(newDiv);
+    document.body.appendChild(mainDiv);
+    document.getElementById(`newdiv${i}`).addEventListener('click', newPage);
+  }
+  let btn = document.getElementById('back-button');
+  if(btn !== null) {
+    btn.remove();
+  }
+}
+
+function removeList() {
+  let x = document.getElementById('mainDiv');
+  let y = document.getElementById('jsonpScriptId');
+  if(x !== null && typeof(x) !== 'undefined') {
+    x.remove(); //document.body.removeChild(x);
+  }
+  if(y !== null && typeof(y) !== 'undefined') {
+    y.remove(); //document.head.removeChild(y);  
+  }
+}
+
+function createBackButton() {
+  let btnBack = document.createElement('button');
+  btnBack.id = 'back-button';
+  btnBack.innerHTML = '<- Back to Results';
+  document.body.appendChild(btnBack);
+  document.getElementById('back-button').addEventListener('click', rebuildList);
+}
+
+function newPage() {
+  removeList();
+  createBackButton();
+  let i = (this.id).slice(-1, this.id.length);
+  let mainDiv = document.createElement('div');
+  mainDiv.id = 'mainDiv';
+  let newDiv = document.createElement('div');
+  let newDesc = arrResults[i].description;
+  
+  ///modify description 
+  newDesc = newDesc.replace(/\s(href=")[^"]+"/g, '');
+  newDesc = newDesc.replace(/(<a)\s/g, "<span ");
+  newDesc = newDesc.replace(/(<\/a>)/g, "</span>");
+  newDesc = newDesc.replace(/\s(class=")[^"]+"/g, '');
+  console.log(newDesc); 
+  ////////////////////////////
+  
+  newDiv.innerHTML = newDesc;
+  document.body.appendChild(mainDiv);
+  mainDiv.appendChild(newDiv);
+
+  //let tags = document.getElementsByTagName('a');
+  //for(let i = 0; i < tags.length;i++) {
+  //  
+  //}
+  //console.log(tags);
+}
+
+function rebuildList() {
+  let x = document.getElementById('mainDiv');
+  if(x !== null && typeof(x) !== 'undefined') {
+    removeList();
+    createList();
+  }
+}
