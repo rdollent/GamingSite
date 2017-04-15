dff5c374f40f077b1c248ab981c76e16b1f6c77c
rdollent <rumbalumba89@gmail.com>
2017-04-15 15:55:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,38 @@
 //79fbb0e3b137824088774e8af118892b8cb22e6e
 
 const key = '79fbb0e3b137824088774e8af118892b8cb22e6e';
-let answer;
 
+const submit = document.getElementsByClassName('submit');
+const search = document.getElementsByClassName('search');
 window.onload = runThis;
 
 function runThis() {
-  const submit = document.getElementById('submit');
-  const search = document.getElementById('search');
-  submit.addEventListener('click', getData);
+
+  submit[0].addEventListener('click', getData);
+  submit[0].classList.remove('fadeOut');
+  search[0].classList.add('searchExpand');
+  search[0].focus();
   
   function getData() {
-    answer = search.value;
+    let answer = search[0].value;
     const jsonpScript = document.createElement('script');
-    jsonpScript.id = 'jsonpScriptId'
+    jsonpScript.id = 'jsonpScriptId';
     jsonpScript.src = `https://www.giantbomb.com/api/search/?api_key=${key}&format=jsonp&json_callback=storeData&query=${answer}&resources=game`;//&field_list=id,name,deck,image,original_release_date,platforms`;
     document.head.appendChild(jsonpScript);
   }
+  
+  
+//animations
+submit[0].addEventListener('click', removePad);
+  
+function removePad() {
+    document.getElementsByClassName('bb')[0].classList.remove('bbPadded');
+}
+  
 }
 
 let arrResults = [];
+
 //note: callback should be in global scope
 function storeData(s) {
   console.log(s);
@@ -35,27 +48,39 @@ function storeData(s) {
 function createList() {
   removeList();
   let mainDiv = document.createElement('div');
-  mainDiv.id = 'mainDiv';
+  mainDiv.classList.add('main');
   for(let i = 0; i < arrResults.length;i++) {
     let newDiv = document.createElement('div');
     newDiv.innerHTML = `<p><h2 class="titles">${arrResults[i].name}</h2></p><p>${arrResults[i].deck}</p>`;
-    newDiv.className = 'results';
+    newDiv.classList.add('results', 'listPad', 'fadeOut');
     newDiv.id = `newdiv${i}`;
     mainDiv.appendChild(newDiv);
     document.body.appendChild(mainDiv);
     document.getElementById(`newdiv${i}`).addEventListener('click', newPage);
+    let j = 0;
+    function myLoop() {
+      setTimeout(function() {
+        document.getElementById(`newdiv${i}`).classList.remove('listPad', 'fadeOut');
+        j++;
+        if(j < arrResults.length) {
+          myLoop();
+        }
+      }, 100);
+    }
+    myLoop();
   }
-  let btn = document.getElementById('back-button');
-  if(btn !== null) {
-    btn.remove();
+  let btn = document.getElementsByClassName('back-button');
+  if(btn[0] !== null) {
+    btn[0].remove();
   }
 }
 
+
 function removeList() {
-  let x = document.getElementById('mainDiv');
+  let x = document.getElementsByClassName('main');
   let y = document.getElementById('jsonpScriptId');
-  if(x !== null && typeof(x) !== 'undefined') {
-    x.remove(); //document.body.removeChild(x);
+  if(x[0] !== null && typeof(x[0]) !== 'undefined') {
+    x[0].remove(); //document.body.removeChild(x);
   }
   if(y !== null && typeof(y) !== 'undefined') {
     y.remove(); //document.head.removeChild(y);  
@@ -64,10 +89,10 @@ function removeList() {
 
 function createBackButton() {
   let btnBack = document.createElement('button');
-  btnBack.id = 'back-button';
+  btnBack.classList.add('back-button', 'fadeOut');
   btnBack.innerHTML = '<- Back to Results';
-  document.body.appendChild(btnBack);
-  document.getElementById('back-button').addEventListener('click', rebuildList);
+  document.body.getElementsByClassName('bb')[0].appendChild(btnBack);
+  document.getElementsByClassName('back-button')[0].addEventListener('click', rebuildList);
 }
 
 function newPage() {
@@ -75,33 +100,52 @@ function newPage() {
   createBackButton();
   let i = (this.id).slice(-1, this.id.length);
   let mainDiv = document.createElement('div');
-  mainDiv.id = 'mainDiv';
+  mainDiv.classList.add('main', 'listPad', 'fadeOut');
   let newDiv = document.createElement('div');
   let newDesc = arrResults[i].description;
   
-  ///modify description 
-  newDesc = newDesc.replace(/\s(href=")[^"]+"/g, '');
-  newDesc = newDesc.replace(/(<a)\s/g, "<span ");
-  newDesc = newDesc.replace(/(<\/a>)/g, "</span>");
-  newDesc = newDesc.replace(/\s(class=")[^"]+"/g, '');
-  console.log(newDesc); 
+  ///modify description
+  if(newDesc !== null) {
+    newDesc = newDesc.replace(/\s(href=")[^"]+"/g, '');
+    newDesc = newDesc.replace(/(<a)\s/g, "<span ");
+    newDesc = newDesc.replace(/(<\/a>)/g, "</span>");
+    newDesc = newDesc.replace(/\s(class=")[^"]+"/g, '');
+    newDesc = newDesc.replace(/(width:\s\d+px)/gi, 'width:80% ');
+  
+    
+    
+  } else {
+    newDesc = "<h2> There is no data for this game </h2>";
+  }
   ////////////////////////////
   
   newDiv.innerHTML = newDesc;
   document.body.appendChild(mainDiv);
   mainDiv.appendChild(newDiv);
 
+  //check to see if there are any <a> anchor tags
   //let tags = document.getElementsByTagName('a');
   //for(let i = 0; i < tags.length;i++) {
   //  
   //}
   //console.log(tags);
+  
+  setTimeout(function() {
+    document.getElementsByClassName('main')[0].classList.remove('listPad', 'fadeOut');
+    document.getElementsByClassName('back-button')[0].classList.remove('fadeOut');
+  },200);
 }
 
 function rebuildList() {
-  let x = document.getElementById('mainDiv');
-  if(x !== null && typeof(x) !== 'undefined') {
+  let x = document.getElementsByClassName('main');
+  if(x[0] !== null && typeof(x[0]) !== 'undefined') {
     removeList();
     createList();
   }
 }
+
+//need:
+//name. platforms, release date.
+//maybe:
+//trailer playing in background
+
